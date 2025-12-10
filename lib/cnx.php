b011317d06fb4d08f6c8dfe5254e90c0484153ca
brief2dev <m.santos283@icloud.com>
2025-12-10 14:18:34 -0600

    cnx

--- lib/cnx.php
+++ lib/cnx.php
@@ -3,13 +3,13 @@ date_default_timezone_set('America/Merida');
 /* $DB = "u665967788_Berel"; 
 $user = "u665967788_Berel_Ticul";
 $pass = "sC*12Bg2"; */
-/* $DB = "ModaMiriam"; 
+$DB = "Miriam"; 
 $user = "root";
-$pass = "Blitzc0de"; */
+$pass = "Blitzc0de";
 
-$DB = "izeek_Miriam"; 
+/* $DB = "izeek_Miriam"; 
 $user = "root";
-$pass = "Server*90";
+$pass = "Server*90"; */
 
 $conexion = new mysqli("localhost", $user, $pass, $DB);
 $conexion->set_charset("utf8");
--- lib/conexion.php
+++ lib/conexion.php
@@ -3,14 +3,14 @@ date_default_timezone_set('America/Merida');
 /* $DB = "u665967788_Berel"; 
 $user = "u665967788_Berel_Ticul";
 $pass = "sC*12Bg2"; */
-/* $DB = "ModaMiriam"; 
+$DB = "Miriam"; 
 $user = "root";
-$pass = "Blitzc0de"; */
+$pass = "Blitzc0de";
 
 
-$DB = "izeek_Miriam"; 
+/* $DB = "izeek_Miriam"; 
 $user = "root";
-$pass = "Server*90";
+$pass = "Server*90"; */
 try{
 	$base_de_datos = new PDO('mysql:host=localhost;dbname=' . $DB, $user, $pass);
 	$base_de_datos->query("set names utf8;");
